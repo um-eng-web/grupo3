81882154ed9bbc8c2302b27d50e61e0b3957ccf8
CatarinaCardoso-pg30462 <pg30462@alunos.uminho.pt>
2016-03-14 23:22:01 +0000

    Registar e login do user

--- main.rb
+++ main.rb
@@ -1,12 +1,31 @@
 require_relative 'user.rb'
 class Main
-	puts "Registe-se no sistema:"
-	puts "Nome:"
-	nome=gets.chomp
-	puts "Email:"
-	mail=gets.chomp
-	puts "Password:" 
-	pass=gets.chomp
-	user=User.new(nome,pass, mail, 0)
-	puts user.getNome
+	def registaUser()
+		puts "Registe-se no sistema:"
+		puts "Nome:"
+		nome=gets.chomp
+		puts "Email:"
+		mail=gets.chomp
+		puts "Password:" 
+		pass=gets.chomp
+		user=User.new(nome,pass, mail, 0)
+		@listUser.push(user)
+	end
+	
+	def loginUser()
+		puts "Login:"
+		puts "Username:"
+		nome=gets.chomp
+		puts "Password:"
+		pass=gets.chomp
+		i=0
+		begin
+			user=listUser.at(i)
+			if user.nome == nome && user.pass ==pass
+				return true
+			end
+			i+=1
+		end while i<listUser.size
+		return false
+	end
 end
