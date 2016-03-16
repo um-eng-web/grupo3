0b3dfe237d730cf89639c32635f4b634a1041b50
CatarinaCardoso-pg30462 <pg30462@alunos.uminho.pt>
2016-03-16 23:16:24 +0000

    Alteração Main

--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
+require_relative 'user.rb'
+require_relative 'bookie.rb'
+require_relative 'team.rb'
+require_relative 'game.rb'
+require_relative 'bet.rb'
+require 'date'
+
+class Main
+	
 	def initialize()
 		
 		$listUser=Array.new
@@ -28,6 +37,169 @@
 		$listJogos.push(Game.new(DateTime.new(2016,4,1,20,45,00),$listEquipas.at(2),$listEquipas.at(3),2,3,1,$listBookie.at(1)))
 		$listJogos.push(Game.new(DateTime.new(2016,4,1,20,45,00),$listEquipas.at(4),$listEquipas.at(5),2,3,1,$listBookie.at(0)))	
 		
+	end
+	
+	def registaUser()
+		puts "Registe-se no sistema:"
+		puts "Nome:"
+		nome=gets.chomp
+		puts "Email:"
+		mail=gets.chomp
+		puts "Password:" 
+		pass=gets.chomp
+		user=User.new(nome,pass, mail, 0)
+		$listUser.push(user)
+	end
+	
+	def loginBookie()
+		puts "Login:"
+		puts "Username:"
+		nome=gets.chomp
+		puts "Password:"
+		pass=gets.chomp
+		i=0
+		begin
+			bookie=$listBookie.at(i)
+			if bookie.getNome == nome && bookie.getPass ==pass
+				@utilizador=bookie
+				return true
+			end
+			i+=1
+		end while i<$listBookie.size
+		return false
+	end
+		
+
+	
+	def menuBookie()
+		puts "Menu"
+		puts "1)Ver Jogos"
+		puts "2)Seguir Jogo"
+		puts "3)Criar Jogo"
+		puts "4)Terminar Jogo"
+		puts "5)Mudar odd"
+		puts "6)Ver Notificações"
+		puts "0)Sair"
+		
+		i=gets.to_i
+		if i==1
+			listarJogos()
+			menuBookie()
+		elsif i==2
+			seguirJogo()
+			menuBookie()
+		elsif i==3
+			criarJogo()
+			menuBookie()
+		elsif i==4
+			terminarJogo()
+			menuBookie()
+		elsif i==5
+			changeOdd()
+			menuBookie()
+		elsif i==6
+			verNotificacoes()
+			menuBookie()
+		elsif i==0
+			return 0
+		else
+			menuBookie()
+		end
+	end
+	
+
+	def seguirJogo()
+		t=true
+		i=0
+		begin
+			listarJogos()
+			puts "Escolha o jogo a seguir:"
+			x=gets.to_i
+			i=0
+			begin
+				if $listJogos.at(i).getData()>DateTime.now && $listJogos.at(i).getNumero==x
+					t=false
+					break
+				end
+				i+=1
+			end while i<$listJogos.size
+		end while t
+		jogo=$listJogos.at(i).getSeguidores
+		jogo.push(@utilizador)
+		$listJogos.at(i).setSeguidores(jogo)
+	end
+	
+	
+	def listarEquipas()
+		i=0
+		begin
+			puts i.to_s + " - " + $listEquipas.at(i).to_s
+			i+=1
+		end while i<$listEquipas.size
+	end
+	
+	
+	def menuUserLog()
+		puts "1)Login"
+		puts "2)Registar"
+		i=gets.to_i
+		if i==1
+			f=loginUser()
+			if f==false
+				puts "login errado! Tente novamente"
+				menuUserLog()
+			else 
+				menuUser()
+			end
+		elsif i==2
+			registaUser()
+			menuUserLog
+		else
+			menuUserLog()
+		end
+	end
+	
+	
+	def bookieOrUser()
+		puts "Bem vindo"
+		puts "1)Bookie"
+		puts "2)User"
+		puts "0)Sair"
+		id=gets.to_i
+		
+		if id == 1
+			menuBookieLog()
+		elsif id == 2
+			menuUserLog()
+		elsif id == 0
+			return 0
+		else 
+			bookieOrUser()
+		end
+	end
+	
+	
+	def verSaldo()
+		puts "Saldo: "+@utilizador.getValor.to_s
+	end
+	
+	
+	def listarApostas()
+		list=@utilizador.getApostas
+		i=0
+		begin
+			puts list.at(i)
+			i+=1
+		end while i<list.size
+	end
+	
+	def verNotificacoes()
+		list=@utilizador.getNotificacao
+		i=0
+		begin
+			puts list.at(i)
+			i+=1
+		end while i<list.size
 	end
 	
 	def loginUser()
@@ -230,3 +402,6 @@
 		end while i<$listJogos.size
 	end
 
+end
+	m=Main.new
+	m.bookieOrUser()
